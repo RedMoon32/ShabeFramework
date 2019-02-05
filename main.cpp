3929e5e21cae3b8f33cf48ae7c70e8a2a65ddaad
RedMoon32 <rinat.babichev@gmail.com>
2019-02-05 23:58:28 +0300

    Fixes

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     
-    char hello[] = "Hello from server";
+    char hello[] = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 29\n\n<H1>Hello Fucking world!</H1>";
     
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
