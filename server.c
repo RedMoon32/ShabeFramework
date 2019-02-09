27d2614dd2c2be99ac3d748196e4d9647cac1985
RedMoon32 <rinat.babichev@gmail.com>
2019-02-09 03:43:08 +0300

    fixes

--- server.c
+++ server.c
@@ -4,17 +4,14 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include "server.h"
 
 #define PORT 8080
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket; long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    
-    char *hello = "Hello from server";
-    
-    // Creating socket file descriptor
+
+int init(struct sockaddr_in* address){
+
+    int server_fd;
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("In socket");
@@ -22,14 +19,14 @@ int main(int argc, char const *argv[])
     }
     
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address->sin_family = AF_INET;
+    address->sin_addr.s_addr = INADDR_ANY;
+    address->sin_port = htons( PORT );
     
-    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    memset(address->sin_zero, '\0', sizeof address->sin_zero);
     
     
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    if (bind(server_fd, address, sizeof(*address))<0)
     {
         perror("In bind");
         exit(EXIT_FAILURE);
@@ -39,10 +36,19 @@ int main(int argc, char const *argv[])
         perror("In listen");
         exit(EXIT_FAILURE);
     }
-    while(1)
-    {
+    return server_fd;
+}
+
+void server_listen(int server_fd,struct sockaddr *address){
+     int addrlen = sizeof(address);
+     int new_socket;
+     long valread;
+     char *hello = "Hello from server";
+        
+     while(1)
+     {
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        if ((new_socket = accept(server_fd, address, (socklen_t*)&addrlen))<0)
         {
             perror("In accept");
             exit(EXIT_FAILURE);
@@ -54,6 +60,17 @@ int main(int argc, char const *argv[])
         write(new_socket , hello , strlen(hello));
         printf("------------------Hello message sent-------------------\n");
         close(new_socket);
-    }
+      }
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_fd;
+    struct sockaddr_in address;
+    
+    
+    server_fd = init(&address);
+    server_listen(server_fd,&address);
+
     return 0;
 }
---
+++ server.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int init(struct sockaddr_in* address);
